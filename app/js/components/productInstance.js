9453410b68db12b120129f0b059c74904451b56d
LeoEatle <liuyitao811@hotmail.com>
2016-08-09 00:05:35 +0800

    V1.5 This is a big commit! There is a very important part which confused me for a long time That is the error Uncaught TypeError: Cannot read property '__reactInternalInstance$td5jlimx8c5yb24vl9v6os9k9' of null This is because when I import the component defined by myself, I use import {ProductInstance} from '../component/ProductInstance' But it's wrong. When the component use 'export default ProductInstance', we dont need to add {}. Just use import ProductInstance from '../component/ProductInstance' --------------------------------------- The update includes the ServiceDetail.js and some pictures And some adust for models And the routers -------------------------------------- Fuck React.js. I want to develop iOS! or vue.js !

--- app/js/components/productInstance.js
+++ app/js/components/productInstance.js
@@ -14,22 +14,32 @@ import {
 } from 'amazeui-react'
 
 
-
 const ProductInstance = React.createClass({
+  //这个函数是为了获取拼接URL
+  geturl: function () {
+    let userid = this.props.product.id.split(":")[0];
+    let serviceid = this.props.product.id.split(":")[1];
+    return ('#/details/' + userid + '/' + serviceid);
+  },
+
+
   render: function () {
     const span_style = {
-      
+
     }
     return (
-      <Col md={3} xs={6}>
-        <Thumbnail className="thumbnail" src={this.props.product.imgUrl} alt={this.props.product.name}>
-          <h3>{this.props.product.name}</h3>
-          <p>{this.props.product.description}</p>
-          <p>
-            <span><Icon icon="star" button  />  加入收藏</span>
-          </p>
+
+      <Col className="productInstance" md={3} xs={6}>
+        <Thumbnail className="thumbnail" href={this.geturl()} src={this.props.product.imgUrl} alt={this.props.product.name}>
+
         </Thumbnail>
+        <h3>{this.props.product.name}</h3>
+        <p>{this.props.product.description}</p>
+        <p>
+          <span><Icon icon="star" button/>  加入收藏</span>
+        </p>
       </Col>
+
     );
   }
 });
